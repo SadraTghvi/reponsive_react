4b6161d6e9039dc2be290115dceffb7772fc2f31
sadra <sadrataghavi1383@gmail.com>
2021-07-15 18:49:23 +0430

    initial setup

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React,{useState,useEffect} from 'react';
+import ReactDOM from 'react-dom';
+
+import './style.css';
+
+
+function Main(){
+    return(
+        <div></div>
+    )
+}
+
+ReactDOM.render(
+    <Main />,
+  document.getElementById('root')
+);
