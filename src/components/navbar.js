5b019ed808e7eaee7eb4c60cb51dfda798e87106
sadra <sadrataghavi1383@gmail.com>
2021-07-15 21:52:19 +0430

    aded usestate and ...

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -4,6 +4,11 @@ import {Link} from "react-router-dom"
 
 
 function Navbar() {
+    const [click,setClick] = useState(false)
+
+    const handleClick = () => setClick(!click);
+    const closeMobileMenu = () => setClick(false);
+    
     return (
         <>
             <nav className="navbar">
@@ -11,6 +16,22 @@ function Navbar() {
                     <Link to="/" className="navbar-logo">
                         TRAVEL <i className="fab fa-typo3"></i>
                     </Link>
+                    <div className="menu-icon" onClick={handleClick}>
+                        <i className={click ? "fas fa-times" : "fas fa-bars"}></i>
+                    </div>
+                    <ul className={click ? "nav-menu active" : "nav-menu"}>
+                        <li className="nav-item">
+                            <Link to="/Services" className="nav-links" onClick={closeMobileMenu}>
+                                Services
+                            </Link>
+                            <Link to="/Products" className="nav-links" onClick={closeMobileMenu}>
+                                Products
+                            </Link>
+                            <Link to="/sign-up" className="nav-links" onClick={closeMobileMenu}>
+                                Home
+                            </Link>
+                        </li>
+                    </ul>
                 </div>
             </nav>
         </>
