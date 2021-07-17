b676863b79d7b587e208e7721776a78c977df8ea
sadra <sadrataghavi1383@gmail.com>
2021-07-17 21:56:49 +0430

    added hero section css part

---
+++ src/components/Pages/Home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import "../../style.css"
+
+import HeroSection from "../heroSection"
+
+const Home = () =>{
+    return(
+        <>
+            <HeroSection></HeroSection>
+        </>
+    )
+}
+
+export default Home
