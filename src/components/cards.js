54f2b23e34b11ba8aea6a94a123b46bc5522d82f
sadra <sadrataghavi1383@gmail.com>
2021-07-19 19:32:03 +0430

    adding some cards to body

--- src/components/cards.js
+++ src/components/cards.js
@@ -9,11 +9,26 @@ function Cards() {
             <h1>Check Out These Epic Destinations</h1>
             <div className="cards__container">
                 <div className="cards__wrapper">
-                    <ul className="cards__items">
-                        <CardItem src="images/img-9.jpg" text="Explore The Hidden Waterf Deep Inside The Amazon Jungleall"
+                    <div className="cards__items">
+                        <CardItem src="images/img-9.jpg" text="Explore The He Amazon Jungleall"
                         label="Adventure"
                         path="/services" />
-                    </ul>
+                        <CardItem src="images/img-3.jpg" text="Explore Tidhe Amazon Jungleall"
+                        label="Explore"
+                        path="/services" />
+                        <CardItem src="images/img-4.jpg" text="Explore The He The Amazon Jungleall"
+                        label="Nature"
+                        path="/services" />
+                        <CardItem src="images/img-5.jpg" text="Explore The HiIne Amazon Jungleall"
+                        label="Explore"
+                        path="/services" />
+                        <CardItem src="images/img-7.jpg" text="Explore  The Amazon Jungleall"
+                        label="Nature"
+                        path="/services" />
+                        <CardItem src="images/img-8.jpg" text="Explore  The Amazon Jungleall"
+                        label="Desurt"
+                        path="/services" />
+                    </div>
                 </div>
             </div>
         </div>
